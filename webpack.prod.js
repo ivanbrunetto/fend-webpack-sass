092f7ed02e4735f66bfc3857e05d9d944025f9ad
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-07-15 06:21:56 -0300

    add serviceworker for offline support

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const WorkBoxPlugin = require('workbox-webpack-plugin')
 
 module.exports = merge(common, {
     mode: 'production',
@@ -14,5 +15,6 @@ module.exports = merge(common, {
     },
     plugins: [
         new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new WorkBoxPlugin.GenerateSW(),
     ]
 })
